484f7a097b01cf474af401ae0df0c35d8c35ae8b
Ethan Borkosky <ethan.borkosky@gmail.com>
2019-09-25 11:39:39 -0400

    update main.js

--- js/main.js
+++ js/main.js
@@ -1,35 +1,59 @@
+//api key and url
 var key = "2510cc4345ae2fb5c4f9c8f0053faa8c";
 
-let subButton = document.getElementById("subButton");
+//declare variables
+let Btn = document.getElementById("subButton");
 let subInput = document.getElementById("userInput");
-let cityName = document.getElementById("city");
+let cityName = document.getElementById("City");
 let kelvin = document.getElementById("kelvin");
 let fh = document.getElementById("fh");
 let cel = document.getElementById("cel");
 let condition = document.getElementById("condition");
 let seasonDisplay = document.getElementById("season");
 
-userZip = document.getElementById('userInput').value
+//btn addeventlistener
+Btn.addEventListener('click', getWeather);
 
-subButton.addEventListener("click", submitted);
 
-function submitted() {
-    if (userZip === ''){    
-    } else {
-        let requestURL = "api.openweathermap.org/data/2.5/weather?zip=" + userZip + "&appid=" + key; 
-        let request = new XMLHttpRequest(); 
-        
-        request.open('GET', requestURL);
-        request.responseType = 'json';
-        request.send();
-    }
+//async function
+function getWeather() {
+
+    //get user value
+    userZip = document.getElementById('userInput').value;
+    var URL = "https://api.openweathermap.org/data/2.5/weather?zip=" + userZip + "&appid=" + key;
+    console.log(userZip);
+    console.log(URL);
+
+    fetch(URL)
+        .then(function (response) {
+            console.log(response);
+            return response.json()
+
+        })
+        .then(function (json) {
+            console.log(json);
+            let city = json.name;
+            cityName.innerHTML = city;
+
+            let tempK = Math.round(json.main.temp);
+            kelvin.innerHTML = tempK + "°";
+
+            let tempF = Math.round(((json.main.temp - 273.15) * 9 / 5) + 32);
+            fh.innerHTML = tempF + "°";
+
+            let tempC = Math.round(json.main.temp - 273.15);
+            cel.innerHTML = tempC + "°";
+
+            let cond = json.weather[0].description;
+            condition.innerHTML = cond;
+
+            let icon = json.weather[0].icon;
+            let iconurl = "http://openweathermap.org/img/w/" + icon + ".png";
+            $('#icon').attr('src', iconurl);
+        })
 }
-console.log(userZip);
 
 
-
-
-    
 
 
 
